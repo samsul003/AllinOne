899e63fa8a32e2c9a7944797d42959b0cd358b4c
Sajidur <sajidur.rahman@particulate.me>
2015-11-11 17:37:38 +0100

    no message

--- AllInOne/settings.py
+++ AllInOne/settings.py
@@ -68,6 +68,11 @@ TEMPLATES = [
         },
     },
 ]
+STATIC_URL = "/static/"
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static"),
+)
 
 WSGI_APPLICATION = 'AllInOne.wsgi.application'
 
--- AllInOne/urls.py
+++ AllInOne/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from main_app import views as main_app_views
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', main_app_views.IndexView.as_view(), name='home')
 ]
